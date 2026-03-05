a14969b681fb5240e166956fc2508a27d4d407aa
pauli <pjseoane@gmail.com>
2026-03-04 22:10:46 -0300

    Using pjs_qlab

--- app/main.py
+++ app/main.py
@@ -7,7 +7,8 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 #from core.services.YahooPriceFetcher import YahooPriceFetcher as price_fetcher
 from pjs_qlab.data.YahooPriceFetcher import YahooPriceFetcher as price_fetcher
-#test cambio git2
+
+
 # ── Data fetching ──────────────────────────────────────────────────────────────
 df=pd.DataFrame()
 @st.cache_resource(ttl=300)  # cache for 5 minutes
