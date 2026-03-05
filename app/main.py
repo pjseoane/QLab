94aa8316d6934166f00bee57e51b1f831c1bbdb9
pauli <pjseoane@gmail.com>
2026-03-04 23:43:06 -0300

    Using pjs_qlab from git

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 #from core.services.YahooPriceFetcher import YahooPriceFetcher as price_fetcher
 from pjs_qlab.data.YahooPriceFetcher import YahooPriceFetcher as price_fetcher
+from pjs_qlab.data.cYahoo_API import cYahoo_API
 
 
 # ── Data fetching ──────────────────────────────────────────────────────────────
