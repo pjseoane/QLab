336ceabeeaae977d5d9df38a1db05bfbfef6a135
Paulino Seoane <pjseoane@users.noreply.github.com>
2026-03-18 09:34:07 -0300

    Eliminacion de lineas #

--- main-navigation.py
+++ main-navigation.py
@@ -1,16 +1,17 @@
 import streamlit as st
 
-welcome    = st.Page("pages/etfs.py", title="Welcome", icon="👋")
+welcome    = st.Page("pages/welcome.py", title="Home", icon=":material/home:")
 dashboard  = st.Page("pages/dashboard.py", title="Dashboard", icon="📊")
 ticker   = st.Page("pages/single_ticker.py", title="Ticker", icon="⚙️")
 portfolios = st.Page("pages/portfolios.py", title="Portfolios", icon="📋")
 etf=st.Page("pages/etf.py", title="ETFs Components", icon=":material/graph_5:")
-
+settings = st.Page("pages/settings.py", title="Settings", icon=":material/settings:")
 
 # grouped navigation
 pg = st.navigation({
     "Main": [welcome, dashboard],
-    "Config": [ticker, portfolios,etf],
+    "Markets": [ticker, portfolios,etf],
+    "Settings": [settings]
 })
 pg.run()
 
