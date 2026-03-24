c17d6b6164d1ffe1cad8732125233df0e43f990e
pauli <pjseoane@gmail.com>
2026-03-23 22:08:08 -0300

    zScore

--- main-navigation.py
+++ main-navigation.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-welcome    = st.Page("pages/welcome.py", title="Home", icon=":material/home:")
+home    = st.Page("pages/home.py", title="Home", icon=":material/home:")
 dashboard  = st.Page("pages/dashboard.py", title="Dashboard", icon="📊")
 ticker   = st.Page("pages/single_ticker.py", title="Ticker", icon="⚙️")
 portfolios = st.Page("pages/portfolios.py", title="Portfolios", icon="📋")
@@ -9,7 +9,7 @@ settings = st.Page("pages/settings.py", title="Settings", icon=":material/settin
 
 # grouped navigation
 pg = st.navigation({
-    "Main": [welcome, dashboard],
+    "Main": [home, dashboard],
     "Markets": [ticker, portfolios,etf],
     "Settings": [settings]
 })
